35cecc83982a251478ad5b90a08f5317fe6670f4
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-23 22:36:36 -0700

    - Read Words from File

--- ClosurePractice/ViewController.swift
+++ ClosurePractice/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let path = Bundle.main.path(forResource: "words", ofType: "rtf")!
+        let url = URL(fileURLWithPath: path)
+        if let fileContents = try? String(contentsOf: url) {
+            allWords = fileContents.components(separatedBy: "\\\n")
+        }
+        
+        startGame()
     }
-
-
+    
+    func startGame() {
+        let word = allWords.randomElement()
+        title = word
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 }
 
